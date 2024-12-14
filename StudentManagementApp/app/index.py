b7e22dcbe7d56b6563234d679d76f9939f7a6ef6
Trung Hậu <tthau2004@gmail.com>
2024-12-14 18:17:44 +0700

    Update models

--- StudentManagementApp/app/index.py
+++ StudentManagementApp/app/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask.globals import app_ctx
+
 
 app = Flask(__name__)
 
---
+++ StudentManagementApp/app/models.py
@@ -0,0 +1,102 @@
+import random
+from sqlalchemy.dialects.mysql import DATETIME
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Enum, intersect
+
+from app import db, app
+from enum import Enum as RoleEnum
+import hashlib
+from flask_login import UserMixin
+
+
+class UserRole(RoleEnum):
+    ADMIN = 1,
+    TEACHER = 2,
+    STAFF = 3
+
+
+class Grade(RoleEnum):
+    Grade_10 = 1
+    Grade_11 = 2
+    Grade_12 = 3
+
+
+class User(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    username = Column(String(100), nullable=False, unique=True)
+    password = Column(String(100), nullable=False)
+    birthday = Column(DATETIME, nullable=False)
+    address = Column(String(255), nullable=False)
+    sex = Column(Integer, nullable=False)
+    email = Column(String(100), nullable=False)
+    user_role = Column(Enum(UserRole))
+
+
+class HocSinh(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    fullname = Column(String(100), nullable=False)
+    sex = Column(Integer, nullable=False)
+    birthday = Column(DATETIME, nullable=False)
+    address = Column(String(255), nullable=False)
+    email = Column(String(100), nullable=False)
+    id_bang_diem = relationship('BangDiem', backref='hoc_sinh', lazy=False)
+
+
+class BangDiem(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    mon_hoc = relationship('MonHoc', backref='bang_diem', lazy=False)
+    hoc_ky = relationship('HocKy', backref='bang_diem', lazy=True)
+    id_hoc_sinh = Column(Integer, ForeignKey(HocSinh.id), nullable=False)
+
+
+class MonHoc(db.Model):
+    ma_mon_hoc = Column(Integer, primary_key=True, autoincrement=True)
+    ten_mon_hoc = Column(String(100), nullable=False)
+    ma_bang_diem = Column(Integer, ForeignKey(BangDiem.id), nullable=False)
+
+
+class Classroom(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), nullable=False)
+    si_so = Column(Integer, nullable=False)
+    grade = Column(Enum(Grade))
+
+
+class HocKy(db.Model):
+    ma_hk = Column(String(50), primary_key=True)
+    nam_hoc = Column(String(50), nullable=False)
+    id_bang_diem = Column(Integer, ForeignKey(BangDiem.id), nullable=False)
+
+
+class ChiTietDiem(db.Model):
+    id = Column(String(50), primary_key=True)
+    type = Column(String(50), nullable=False)
+    value = Column(Float, nullable=False)
+    note = Column(String(100), nullable=True)
+
+
+class Lop_MonHoc(db.Model):
+    ma_lop = Column(Integer, ForeignKey(Classroom.id), primary_key=True)
+    ma_mh = Column(Integer, ForeignKey(MonHoc.ma_mon_hoc), primary_key=True)
+
+
+class HocSinh_Lop(db.Model):
+    ma_hs = Column(Integer, ForeignKey(HocSinh.id), primary_key=True)
+    ma_lop = Column(Integer, ForeignKey(Classroom.id), primary_key=True)
+
+
+class User_MonHoc():
+    id_user = Column(Integer, ForeignKey(User.id), primary_key=True)
+    ma_lop = Column(Integer, ForeignKey(Classroom.id), primary_key=True)
+    ma_mh = Column(Integer, ForeignKey(MonHoc.ma_mon_hoc), primary_key=True)
+
+
+class ChiTietDiem_BangDiem(db.Model):
+    ma_bang_diem = Column(Integer, ForeignKey(BangDiem.id), primary_key=True)
+    ma_diem = Column(String(50), ForeignKey(ChiTietDiem.id), primary_key=True)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
