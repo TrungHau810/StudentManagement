6b2382bbb135c0eb97ad84608a2ea6110d72be58
Trung Hậu <tthau2004@gmail.com>
2024-12-17 16:57:53 +0700

    Update login and logout

--- StudentManagementApp/app/dao.py
+++ StudentManagementApp/app/dao.py
@@ -11,8 +11,7 @@ from flask_login import current_user
 def auth_user(username, password):
     password = str(hashlib.md5(password.encode('utf-8')).hexdigest())
 
-    return User.query.filter(User.username.__eq__(username.strip()),
-                             User.password.__eq__(password)).first()
+    return User.query.filter(User.username.__eq__(username.strip()), User.password.__eq__(password)).first()
 
 
 
--- StudentManagementApp/app/index.py
+++ StudentManagementApp/app/index.py
@@ -2,9 +2,7 @@ from flask import Flask, render_template, request, redirect
 import dao
 from app import app, db, login
 from app.models import UserRole
-from flask_login import login_user
-
-app = Flask(__name__)
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route("/")
@@ -25,6 +23,13 @@ def login_view():
     return render_template('login.html')
 
 
+@app.route("/logout")
+def logout_process():
+    logout_user()
+    return redirect('/login')
+
+
+
 @login.user_loader
 def load_user(user_id):
     return dao.get_user_by_id(user_id)
@@ -33,6 +38,8 @@ def load_user(user_id):
 @app.route("/scores-input")
 def scores_input_view():
     return render_template('scores-input.html')
+
+
 
 
 if __name__ == "__main__":
